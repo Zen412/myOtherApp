b35324513e15da3d59cf2902d1fa01ce9d47ebe5
zakes <zenzelenkabinde@gmail.com>
2023-03-22 13:15:49 +0200

    Initial commit

---
+++ src/main/java/com/SpringBoot/Demo/myOtherApp/MyOtherAppApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.Demo.myOtherApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOtherAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyOtherAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringBoot/Demo/myOtherApp/MyOtherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.Demo.myOtherApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyOtherAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
